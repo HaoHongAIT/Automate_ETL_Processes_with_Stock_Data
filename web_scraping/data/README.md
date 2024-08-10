50d523b3905f2d70afd3fcb6a887fdb45b93dde5
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2024-08-10 21:30:24 +0700

    240810

---
+++ web_scraping/data/README.md
@@ -0,0 +1,9 @@
+
+|Terminology|mean|
+|:---------:|----|
+|date|ngày|
+|price|Giá (nghìn VNĐ)|
+|rate_change|tỉ lệ giá thay đổi so với ngày trước đó|
+||GD khớp lệnh|
+||GD thỏa thuận|
+|||
--- web_scraping/get_data.py
+++ web_scraping/get_data.py
@@ -13,24 +13,29 @@ def read_doc():
     pass
 
 
-def somthig(df):
+def list_to_csv(lst, stock_code) -> dict:
+    dic = {"date": [], "closing_price": [], "adjusting_price": [], "rate_change": [],
+           "order_matching_volumn": [], "order_matching_value": [],
+           "block_trade_volume": [], "block_trade_value": [],
+           "open_price": [], "max_price": [], "min_price": []}
+
     for i in range(0, len(lst), 11):
-        df['date'].append(lst[i])
-        df['closed_price'].append(lst[i+1])
-        df['adjusted_price'].append(lst[i+2])
-        df['col 1'].append(lst[i+3])
-        df['col 2'].append(lst[i+4])
-        df['col 3'].append(lst[i+5])
-        df['col 4'].append(lst[i+6])
-        df['col 5'].append(lst[i+7])
-        df['col 6'].append(lst[i+8])
-        df['col 7'].append(lst[i+9])
-        df['col 8'].append(lst[i+10])
+        dic['date'].append(lst[i])
+        dic['closing_price'].append(lst[i+1])
+        dic['adjusting_price'].append(lst[i+2])
+        dic['rate_change'].append(lst[i+3])
+        dic['order_matching_volumn'].append(lst[i+4])
+        dic['order_matching_value'].append(lst[i+5])
+        dic['block_trade_volume'].append(lst[i+6])
+        dic['block_trade_value'].append(lst[i+7])
+        dic['open_price'].append(lst[i+8])
+        dic['max_price'].append(lst[i+9])
+        dic['min_price'].append(lst[i+10])
+    pd.DataFrame(dic).to_csv(f"./data/{stock_code}.csv")
 
 
 def get_Data(browser, stock_code, index) -> list:
-    """
-    get data from table at url
+    """get data from table at url
     """
     data = []
     browser.get(
@@ -45,7 +50,8 @@ def get_Data(browser, stock_code, index) -> list:
                 By.XPATH, "/html/body/form/div[3]/div[2]/div[1]/div[3]/div/div[3]/div[3]").click()
             sleep(1)
             print(len(data))
-        except:
+        except RuntimeError:
+            print("crawl end")
             break
     browser.close()
     return data
@@ -58,5 +64,4 @@ if __name__ == '__main__':
     service = Service(executable_path='./chromedriver.exe')
     browser = webdriver.Chrome(service=service, options=options)
     lst = get_Data(browser=browser, stock_code='fpt', index=1)
-    ## bat dau luu
-    
+    dic = list_to_csv(lst=lst, stock_code='fpt')
