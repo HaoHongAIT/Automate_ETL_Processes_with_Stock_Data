c634c9b08561cac29271f06d759c182e0ece120b
kiemmoon <kiemmoonwork@gmail.com>
2024-08-16 19:15:46 +0700

    Changes by hongt

---
+++ pipeline.py
@@ -0,0 +1,35 @@
+from web_scraping import  WebScraping
+from preprocessing import preprocess
+
+
+
+
+if __name__ == '__main__':
+
+    # WEB SCRAPING
+    ticker, index = 'fpt', [1]
+    url = f"https://s.cafef.vn/lich-su-giao-dich-{ticker}-{index}.chn"
+    fpt_crawler = WebScraping.Crawl(ticker, url, index)
+
+    ## get data
+    fpt_data = fpt_crawler.get_Data(time=None)
+    ## save
+    fpt_crawler.save(lst=fpt_data, filename="fpt")
+
+    # PREPROCESS
+    ##
+
+    ## save
+    # FORECAST
+
+
+    # FORECAST orecast
+
+    # implement
+
+    # automatation
+
+
+    lst = get_Data(browser=browser, ticker='fpt', index=1)
+    list_to_csv(lst=lst, dic=dic, ticker='fpt')
+    list_to_csv = preprocess.preprocess()
---
+++ web_scraping/WebScraping.py
@@ -0,0 +1,64 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+import pandas as pd
+from preprocessing import preprocess
+
+
+
+class Crawl:
+    def __init__(self, ticker, url, index):
+        self.dic = {"date": [], "closing_price": [], "adjusting_price": [], "rate_change": [],
+                    "order_matching_volume": [], "order_matching_value": [],
+                    "block_trade_volume": [], "block_trade_value": [],
+                    "open_price": [], "high": [], "low": []}
+        self.ticker_info = {'ticker': ticker, 'index': [i for i in index], 'url': url}
+        options = Options()
+        options.headless = False
+        options.add_argument("--window-size=1920,1080")
+        service = Service(executable_path='./chromedriver.exe')
+        self.browser = webdriver.Chrome(service=service, options=options)
+
+    def save(self, lst, filename):
+        for i in range(0, len(lst), 11):
+            self.dic['date'].append(lst[i])
+            self.dic['closing_price'].append(lst[i + 1])
+            self.dic['adjusting_price'].append(lst[i + 2])
+            self.dic['rate_change'].append(lst[i + 3])
+            self.dic['order_matching_volume'].append(lst[i + 4])
+            self.dic['order_matching_value'].append(lst[i + 5])
+            self.dic['block_trade_volume'].append(lst[i + 6])
+            self.dic['block_trade_value'].append(lst[i + 7])
+            self.dic['open_price'].append(lst[i + 8])
+            self.dic['high'].append(lst[i + 9])
+            self.dic['low'].append(lst[i + 10])
+        pd.DataFrame(self.dic).to_csv(f"./data/raw/{filename}.csv")
+
+
+    def get_Data(self, time = None) -> list:
+        data = []
+        self.browser.get(self.ticker_info['url'])
+        if time:
+            search_bar = self.browser.find_element(By.ID, 'date-inp-disclosure')
+            self.browser.execute_script(time, search_bar)
+            self.browser.find_element(By.ID, 'owner-find').click()
+            sleep(1)
+
+        class_name = ""
+        while "enable" not in class_name:
+            try:
+                elements = self.browser.find_elements(
+                    By.CSS_SELECTOR, ".render-table-owner td")
+                data += [element.text for element in elements]
+                next_page = self.browser.find_element(By.ID, "paging-right")
+                next_page.click()
+                class_name = next_page.get_attribute("class")
+                sleep(1)
+            except:
+                print("Crawling Completely")
+                break
+        self.browser.close()
+        return data
