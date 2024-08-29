841dee3464a851cbbf844fabda549c65938bc856
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2024-08-29 17:48:37 +0700

    #add database connection

--- ETL/load/connect.py
+++ ETL/load/connect.py
@@ -1,21 +1,18 @@
-import SQLAlchemy
-
-# SQL > database
-# ORM: python > sqlachemy > sql > database
-# 
-
-app.secret_key = '1234567890qwertyuiop'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/news?charset=utf8mb4'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+import sqlite3
 
 
-def create_db(app):
-return SQLAlchemy(app=app)
+def create_sqlite_database(filename):
+    """ create a database connection to an SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(filename)
+        print(sqlite3.sqlite_version)
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
-app = create_app()
-login_manager = LoginManager(app=app)
-root_path = app.root_path
-phobert_model = load_model(root_path)
-# phobert_model = None
-db = create_db(app)
+if __name__ == '__main__':
+    create_sqlite_database("my.db")
