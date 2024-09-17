32cdc989e5e138678d140aa4e6c2a83ad809d410
kiemmoon <kiemmoonwork@gmail.com>
2024-09-17 19:12:32 +0700

    #update transform
    rewrite most of transform.py to fix 'object' type error

--- ETL/transform/transform.py
+++ ETL/transform/transform.py
@@ -1,82 +1,114 @@
 import pandas as pd
 import re
-from datetime import date
+# from datetime import date
 import glob
+import os
 
-TODAY = date.today().strftime(r"%Y-%m-%d")
+
+# TODAY = date.today().strftime(r"%Y-%m-%d")
+def obj_to_int(values):
+    lst = []
+    for value in values:
+        if isinstance(value, str): lst.append(int(value.replace(',', '')))
+        elif isinstance(value, int): lst.append(value),
+    return lst
+
+def obj_to_float(values):
+    lst = []
+    for value in values:
+        if isinstance(value, str): lst.append(float(value.replace(',', '')))
+        elif isinstance(value, int): lst.append(float(value)),
+        elif isinstance(value, float): lst.append(value)
+    return lst
+
+
 
 class Transform:
     def __init__(self):
-        pass
+        self.save_folder = f".\\data\\transformed\\2024-09-14"
+        if not os.path.exists(self.save_folder):
+            os.makedirs(self.save_folder)
 
     def combine_csv(self):
         df1, df2, df3, df4 = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
-        path = glob.glob(f".\\data\\raw\\{TODAY}\\*.csv")
+        # path = glob.glob(f".\\data\\raw\\{TODAY}\\*.csv")
+        path = glob.glob(".\\data\\raw\\2024-09-14\\*.csv")
         for file_path in path:
             tmp = pd.read_csv(file_path)
-            if "_1." in file_path:
-                df1._append(tmp, ignore_index=True)
-                break
-            if "_2." in file_path:
-                df2._append(tmp, ignore_index=True)
-                break
-            if "_3." in file_path:
-                df3._append(tmp, ignore_index=True)
-                break
-            if "_4." in file_path:
-                df4._append(tmp, ignore_index=True)
-                break
+            if "_1." in file_path: df1 = df1._append(tmp, ignore_index=True)
 
+            elif "_2." in file_path: df2 = df2._append(tmp, ignore_index=True)
+
+            elif "_3." in file_path: df3 = df3._append(tmp, ignore_index=True)
+
+            elif "_4." in file_path: df4 = df4._append(tmp, ignore_index=True)
+        csv_info = f"""CSV Info:
+        ---\ndataframe 1:\n{df1.dtypes}\n---
+        ---\ndataframe 2:\n{df2.dtypes}\n---
+        ---\ndataframe 3:\n{df3.dtypes}\n---
+        ---\ndataframe 4:\n{df4.dtypes}\n---
+        """
+        print(csv_info)
         return df1, df2, df3, df4
 
-
-    def trans_price_history(self, df,index=1):
+    def trans_price_history(self, df):
         df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
         df['adjusting_price'] = df['adjusting_price'].replace('--', 'N/A')
-        rate = lambda x: re.search(r'\(([-+]?\d*\.?\d+)\s*%', x).group(1) if pd.notnull(x) else x
+        rate = lambda x: float(re.search(r'\(([-+]?\d*\.?\d+)\s*%', x).group(1)) if pd.notnull(x) else float(x)
         df['rate_change'] = df['rate_change'].apply(rate)
-        df['order_matching_volume'] = [int(value.replace(',', '')) for value in df['order_matching_volume']]
-        df['order_matching_value'] = [float(value.replace(',', '')) for value in df['order_matching_value']]
-        df['block_trade_volume'] = [int(value.replace(',', '')) for value in df['block_trade_volume']]
-        df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_price_history.csv", index=False)
+        df['order_matching_volume'] = obj_to_int(df["order_matching_volume"].tolist())
+        df['block_trade_volume'] = obj_to_int(df["block_trade_volume"].tolist())
+        df['block_trade_value'] = obj_to_float(df["block_trade_value"].tolist())
+        df['order_matching_value'] = obj_to_float(df['order_matching_value'].tolist())
+        # df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_price_history.csv", index=False)
+        df.to_csv(f"{self.save_folder}\\price_history.csv", index=False)
+        print(df.dtypes)
 
 
-    def trans_order_flow_stat(self, df, index=2):
+    def trans_order_flow_stat(self, df):
         df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
-        df['buy_orders'] = [int(value.replace(',', '')) for value in df['buy_orders']]
-        df['buy_volume'] = [int(value.replace(',', '')) for value in df['buy_volume']]
-        df['average_buy_order_volume'] = [float(value.replace(',', '')) for value in df['average_buy_order_volume']]
-        df['sell_orders'] = [int(value.replace(',', '')) for value in df['sell_orders']]
-        df['sell_volume'] = [int(value.replace(',', '')) for value in df['sell_volume']]
-        df['average_sell_order_volume'] = [int(value.replace(',', '')) for value in df['average_sell_order_volume']]
-        df['net_volume'] = [int(value.replace(',', '')) for value in df['net_volume']]
-        df.drop(columns=['rate_change'], inplace= True)
-        print("Transform Complete Order Flow Statistics")
-        df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_order_flow_stat.csv",  index=False)
+        
+        df['buy_orders'] = obj_to_int(df["buy_orders"].tolist())
+        df['buy_volume'] = obj_to_int(df["buy_volume"].tolist())
+        df['average_sell_order_volume'] = obj_to_int(df["average_sell_order_volume"].tolist())
+        df['sell_orders'] = obj_to_int(df["sell_orders"].tolist())
+        df['sell_volume'] = obj_to_int(df["sell_volume"].tolist())
+        df['net_volume'] = obj_to_int(df["net_volume"].tolist())
 
-    def trans_foreign_investors(self, df, index=3):
-        df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
-        df['net_trading_volume'] = [int(value.replace(',', '')) for value in df['net_trading_volume']]
-        df['buy_volume'] = [int(value.replace(',', '')) for value in df['buy_volume']]
-        df['sell_volume'] = [int(value.replace(',', '')) for value in df['sell_volume']]
-        df['remaining_room'] = [int(value.replace(',', '')) for value in df['remaining_room']]
-        df['current_ownership'] = [float(value.replace('%', '')) for value in df['current_ownership']]
-        df.drop(columns=['rate_change'], inplace= True)
-        print("Transform Complete Foreign Investors")
-        df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_foreign_investors.csv",  index=False)
+        df['average_buy_order_volume'] = obj_to_float(df['average_buy_order_volume'])
+        df.drop(columns=['rate_change'], inplace=True)
+        # df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_order_flow_stat.csv",  index=False)
+        df.to_csv(f"{self.save_folder}\\order_flow_stat.csv", index=False)
+        print(df.dtypes)
 
-    def trans_proprietary_trading(self, df, index=4):
+    def trans_foreign_investors(self, df):
         df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
-        df['buy_volume'] = [int(value.replace(',', '')) for value in df['buy_volume']]
-        df['sell_volume'] = [int(value.replace(',', '')) for value in df['sell_volume']]
-        df["net_trading_volume"] = [int(value.replace(',', '')) for value in df["net_trading_volume"]]
-        df['ticker'] = ticker.lower()
-        print("Transform Complete Proprietary Trading")
-        df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_proprietary_trading.csv",  index=False)
+
+        df['net_trading_volume'] = obj_to_int(df['net_trading_volume'].tolist())
+        df['buy_volume'] = obj_to_int(df['buy_volume'].tolist())
+        df['sell_volume'] = obj_to_int(df['sell_volume'].tolist())
+        df['remaining_room'] = obj_to_int(df['remaining_room'].tolist())
+        owership = lambda x: x.replace("%","")
+        df['current_ownership'] = df['current_ownership'].apply(owership)
+        df['current_ownership'] = obj_to_float(df['current_ownership'].tolist())
+        df.drop(columns=['rate_change'], inplace=True)
+        # df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_foreign_investors.csv",  index=False)
+        df.to_csv(f"{self.save_folder}\\foreign_investors.csv", index=False)
+        print(df.dtypes)
+
+    def trans_proprietary_trading(self, df):
+        df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+        df['buy_volume'] = obj_to_int(df['buy_volume'].tolist())
+        df['sell_volume'] = obj_to_int(df['sell_volume'].tolist())
+        df["net_trading_volume"] = obj_to_int(df["net_trading_volume"].tolist())
+        # df.to_csv(f"./data/transformed/{TODAY}/transformed_trans_proprietary_trading.csv",  index=False)
+        df.to_csv(f"{self.save_folder}\\proprietary_trading.csv", index=False)
+        print(df.dtypes)
 
     def run(self):
+
         df1, df2, df3, df4 = self.combine_csv()
-        trans_price_history(df1)
-        trans_foreign_investors(df2)
-        trans_order_flow_stat(df3)
-        trans_proprietary_trading(df4)
+        self.trans_price_history(df1)
+        self.trans_order_flow_stat(df2)
+        self.trans_foreign_investors(df3)
+        self.trans_proprietary_trading(df4)
--- etl_pipeline.py
+++ etl_pipeline.py
@@ -3,7 +3,6 @@ from ETL.transform import *
 from ETL.load import load
 from ETL import *
 import pandas as pd
-
 from ETL.transform.transform import Transform
 
 # stock_codes = pd.read_excel(r'./data/document/code_stock.xlsx')['ticker'].to_list()[0:200]
@@ -11,9 +10,21 @@ from ETL.transform.transform import Transform
 # crawler = MultiThreading(threads=10, code_list=stock_codes)
 # crawler.run(time_range=time_range)
 
-## TRASFORM > Transformed Data
+# TRASFORM > Transformed Data
 transform1 = Transform()
 transform1.run()
 
 #     ## Load > Available Data for Analysis
 #     load.run(ticker=ticker)
+
+
+# import glob
+# import re
+# path = glob.glob(f".\\data\\raw\\2024-09-14\\*.csv")
+# for file_path in path:
+#     df = pd.read_csv(file_path)
+#     df['ticker'] = str(re.search(r'\\[\d\-]+\\([a-zA-Z0-9]+)_\d+\.csv$', file_path).group(1))
+#     df.to_csv(file_path, index=False)
+
+
+
