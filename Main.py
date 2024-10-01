5dabb224c5d812ebf3799df847d69e6e952e48af
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2024-10-01 11:34:45 +0700

    #remove streamlit
    Using Power BI to visualize data instead of Streamlit

--- Main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Stock Prediction App",
-    page_icon="",
-)
-with open('./data/document/main.md', 'r', encoding='utf-8') as file:
-    markdown_content = file.read()
-st.markdown(markdown_content)
